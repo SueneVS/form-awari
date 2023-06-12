d30ea465af36e308c27fc3bd1809dfcf6ead8cdb
Suene <suene.vs@gmail.com>
2023-06-12 15:06:46 -0300

    :lipstick: feat: Estilizaçao CSS do formulario

---
+++ README.md
@@ -0,0 +1,2 @@
+#Prática: Formulário de Contato
+##Esta prática foi realizada durante a aula do curso de Front-End da Awari
--- script.js
+++ script.js
@@ -3,11 +3,11 @@ var form=document.getElementById("contact-form");
 function handleSubmit(event){
     event.preventDefault();
 
-    var name = form.elements["Nome"].value;
-    var email = form.elements["Email"].value;
-    var mensagem = form.elements["Mensagem"].value;
+    var name = form.elements["name"].value;
+    var email = form.elements["email"].value;
+    var message = form.elements["message"].value;
 
-    var resultado = `Nome: ${name} \nE-mail: ${email} \nMensagem: ${mensagem}`;
+    var resultado = `Nome: ${name} \nE-mail: ${email} \nMensagem: ${message}`;
     alert(resultado);
 }
 
